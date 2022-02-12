627e6b1cff5d77aace07033ca9a335c101c55760
amir-shahsahebi <92686170+amir-shahsahebi@users.noreply.github.com>
2022-02-12 23:58:08 +0330

    css added

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let allEpisodes = async () => {
+let container = document.querySelector(".container")
+let grabber = async () => {
     try {
         const res = await axios.get("https://api.tvmaze.com/shows/5/episodes")
         console.log(res.data)
-        return res.data
+        allEpisodes = res.data
+        for (episode of allEpisodes) {
+          let div = document.createElement("div");
+          div.classList.add("div-card");
+          console.log(episode.image.medium);
+          div.style.backgroundImage = episode.image.medium;
+          container.append(div);
+        }
     } catch (err) {
         console.log(err)
     }
 }
-allEpisodes()
-for (episode of allEpisodes){
-    
-}
+grabber()
