fcd9718ba2418c0aaec65b8673f187d110de05fe
amir-shahsahebi <92686170+amir-shahsahebi@users.noreply.github.com>
2022-02-15 14:52:13 +0330

    select structure completed

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let eps = async () => {
     let allepisode = all.data
     
     select.name = "select";
+    let option0 = document.createElement("option");
+    option0.value = "nothing";
+    option0.innerText = "please select witch you want";
+    select.append(option0);
     let option1 = document.createElement("option");
     option1.value = "all";
     option1.innerText = "All episodes";
@@ -40,40 +44,41 @@ let grabber = async () => {
         const res = await axios.get("true.json")
         // console.log(res.data)
         allEpisodes = res.data
-        for (episode of allEpisodes) {
-            //define card
-          let div = document.createElement("div");
-          div.classList.add("div-card");
-        //   console.log(episode.image.medium);
-          div.style.backgroundImage = `url(${episode.image.medium})`;
-          div.style.backgroundSize = `cover`;
-        //   div.style.margin= "1rem"
-          container.append(div);
-          let divNum= document.createElement("div")
-          divNum.innerText= `S0${episode.season}E0${episode.number}`
-          divNum.classList.add("divNum");
-          div.append(divNum)
-          let divName= document.createElement("div")
-          divName.innerText= episode.name
-          divName.classList.add("divName")
-          div.append(divName)
-          divSummary= document.createElement("div")
-          divSummary.innerHTML = episode.summary
-          divSummary.classList.add("divSummary")
-          div.append(divSummary)
-          divScore = document.createElement("span")
-          divScore.innerText = `${episode.rating.average}/10`;
-          let a = document.createElement("a")
-          a.href=episode.url
-          a.classList.add("a")
-          divScore.classList.add("divScore")
-          div.append(divScore)
-          let icon = document.createElement("i")
-          icon.classList.add("fas", "fa-play" , "icon");
-          a.append(icon)
-          div.append(a)
-          // console.log(episode)
-        }
+        creation(allEpisodes);
+        // for (episode of allEpisodes) {
+        //     //define card
+        //   let div = document.createElement("div");
+        //   div.classList.add("div-card");
+        // //   console.log(episode.image.medium);
+        //   div.style.backgroundImage = `url(${episode.image.medium})`;
+        //   div.style.backgroundSize = `cover`;
+        // //   div.style.margin= "1rem"
+        //   container.append(div);
+        //   let divNum= document.createElement("div")
+        //   divNum.innerText= `S0${episode.season}E0${episode.number}`
+        //   divNum.classList.add("divNum");
+        //   div.append(divNum)
+        //   let divName= document.createElement("div")
+        //   divName.innerText= episode.name
+        //   divName.classList.add("divName")
+        //   div.append(divName)
+        //   divSummary= document.createElement("div")
+        //   divSummary.innerHTML = episode.summary
+        //   divSummary.classList.add("divSummary")
+        //   div.append(divSummary)
+        //   divScore = document.createElement("span")
+        //   divScore.innerText = `${episode.rating.average}/10`;
+        //   let a = document.createElement("a")
+        //   a.href=episode.url
+        //   a.classList.add("a")
+        //   divScore.classList.add("divScore")
+        //   div.append(divScore)
+        //   let icon = document.createElement("i")
+        //   icon.classList.add("fas", "fa-play" , "icon");
+        //   a.append(icon)
+        //   div.append(a)
+        //   // console.log(episode)
+        // }
         let all= allEpisodes
         // console.log(all)
         let allFilter = all.filter(x=>x.name.includes("Long") || x.summary.includes("Long") )
@@ -81,6 +86,43 @@ let grabber = async () => {
     } catch (err) {
         console.log(err)
     }
+}
+
+function creation(allEpisodes) {
+  for (episode of allEpisodes) {
+    //define card
+    let div = document.createElement("div");
+    div.classList.add("div-card");
+    //   console.log(episode.image.medium);
+    div.style.backgroundImage = `url(${episode.image.medium})`;
+    div.style.backgroundSize = `cover`;
+    //   div.style.margin= "1rem"
+    container.append(div);
+    let divNum = document.createElement("div");
+    divNum.innerText = `S0${episode.season}E0${episode.number}`;
+    divNum.classList.add("divNum");
+    div.append(divNum);
+    let divName = document.createElement("div");
+    divName.innerText = episode.name;
+    divName.classList.add("divName");
+    div.append(divName);
+    divSummary = document.createElement("div");
+    divSummary.innerHTML = episode.summary;
+    divSummary.classList.add("divSummary");
+    div.append(divSummary);
+    divScore = document.createElement("span");
+    divScore.innerText = `${episode.rating.average}/10`;
+    let a = document.createElement("a");
+    a.href = episode.url;
+    a.classList.add("a");
+    divScore.classList.add("divScore");
+    div.append(divScore);
+    let icon = document.createElement("i");
+    icon.classList.add("fas", "fa-play", "icon");
+    a.append(icon);
+    div.append(a);
+    // console.log(episode)
+  }
 }
 // grabber()
 // let select = document.querySelector("select");
