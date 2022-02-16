b5ca52d282d90d1fe3dd81f387bcebb2b1648069
amir-shahsahebi <92686170+amir-shahsahebi@users.noreply.github.com>
2022-02-16 20:35:41 +0330

    typing effect added

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-let container = document.querySelector(".container")
-let divHeader = document.querySelector(".div-header")
+let text = [
+  "Life's barely long enough to get good at one thing. So be careful what you get good at.",
+  // "The world needs bad men. We keep the other bad men from the door.",
+  // "Trouble with dying later is you've already grown up. Damage is done. It's too late.",
+  // "There is no such thing as forgiveness, people just have short memories.",
+  // "I'd consider myself a realist, all right? But in philosophical terms, I'm what's called a pessimist.",
+  // "If the common good has got to make up fairy tales, then it’s not good for anybody.",
+  // "This place is like somebody’s memory of a town, and the memory is fading. It’s like there was never anything here but a jungle.",
+];
+function typing(){
+  let k = 0;
+  for (let i=0;i<text.length;i++){
+    function type(){
+      if (k < text[i].length) {
+        // console.log("i=",i)
+        // console.log("k=",k)
+      document.querySelector(".dialog-content").innerHTML += text[i].charAt(k);
+      k++;
+      setTimeout(type, 150);
+      }
+    }
+     type();
+  }
+}
+typing()
+
+// var typed = new Typed(".dialog-content", {
+//   Strings: ["hello","see","moon"],
+//   typeSpeed: 100,
+//   backSpeed: 100,
+//   loop: true
+// });
+
+
+
+// window.addEventListener("load",typing)
+// dialogContent.innerText = text[0]
+// easterFun();
+let container = document.querySelector(".container");
+let divHeader = document.querySelector(".div-header");
 let select = document.querySelector("select");
 let eps = async () => {
   try {
     let all = await axios.get("true.json");
-    let allepisode = all.data
-    
+    let allepisode = all.data;
+
     select.name = "select";
     let option0 = document.createElement("option");
     option0.value = "nothing";
@@ -17,76 +55,80 @@ let eps = async () => {
     select.append(option1);
     // console.log(allepisode);
     for (one of allepisode) {
-      // console.log(one)  
-      let option = document.createElement("option")
-      option.value= one.name
+      // console.log(one)
+      let option = document.createElement("option");
+      option.value = one.name;
       option.innerText = `S0${one.season}E0${one.number} - ${one.name}`;
-      select.append(option)
+      select.append(option);
     }
     // divHeader.append(select)
+  } catch (err) {
+    console.log(err);
   }
-   catch (err) {
-    console.log(err)
-  }
-}
-eps()
-    
-    // return all.date;
+};
+eps();
+
+// return all.date;
 // let all = eps().then(data=>data)
 // console.log(all);
 
-
-
-
-let grabber = async () => {
-    try {
-        // const res = await axios.get("https://api.tvmaze.com/shows/5/episodes")
-        const res = await axios.get("true.json")
-        // console.log(res.data)
-        allEpisodes = res.data
-        creation(allEpisodes);
-        // for (episode of allEpisodes) {
-        //     //define card
-        //   let div = document.createElement("div");
-        //   div.classList.add("div-card");
-        // //   console.log(episode.image.medium);
-        //   div.style.backgroundImage = `url(${episode.image.medium})`;
-        //   div.style.backgroundSize = `cover`;
-        // //   div.style.margin= "1rem"
-        //   container.append(div);
-        //   let divNum= document.createElement("div")
-        //   divNum.innerText= `S0${episode.season}E0${episode.number}`
-        //   divNum.classList.add("divNum");
-        //   div.append(divNum)
-        //   let divName= document.createElement("div")
-        //   divName.innerText= episode.name
-        //   divName.classList.add("divName")
-        //   div.append(divName)
-        //   divSummary= document.createElement("div")
-        //   divSummary.innerHTML = episode.summary
-        //   divSummary.classList.add("divSummary")
-        //   div.append(divSummary)
-        //   divScore = document.createElement("span")
-        //   divScore.innerText = `${episode.rating.average}/10`;
-        //   let a = document.createElement("a")
-        //   a.href=episode.url
-        //   a.classList.add("a")
-        //   divScore.classList.add("divScore")
-        //   div.append(divScore)
-        //   let icon = document.createElement("i")
-        //   icon.classList.add("fas", "fa-play" , "icon");
-        //   a.append(icon)
-        //   div.append(a)
-        //   // console.log(episode)
-        // }
-        let all= allEpisodes
-        // console.log(all)
-        let allFilter = all.filter(x=>x.name.includes("Long") || x.summary.includes("Long") )
-        // console.log(allFilter)
-    } catch (err) {
-        console.log(err)
+let grabber = async (target) => {
+  try {
+    // const res = await axios.get("https://api.tvmaze.com/shows/5/episodes")
+    const res = await axios.get("true.json");
+    // console.log(res.data)
+    if (target === "all") {
+      allEpisodes = res.data;
+    } else {
+      allEpisodes = res.data.filter((x) => x.name.includes(target));
+      console.log(allEpisodes);
     }
-}
+
+    creation(allEpisodes);
+    // for (episode of allEpisodes) {
+    //     //define card
+    //   let div = document.createElement("div");
+    //   div.classList.add("div-card");
+    // //   console.log(episode.image.medium);
+    //   div.style.backgroundImage = `url(${episode.image.medium})`;
+    //   div.style.backgroundSize = `cover`;
+    // //   div.style.margin= "1rem"
+    //   container.append(div);
+    //   let divNum= document.createElement("div")
+    //   divNum.innerText= `S0${episode.season}E0${episode.number}`
+    //   divNum.classList.add("divNum");
+    //   div.append(divNum)
+    //   let divName= document.createElement("div")
+    //   divName.innerText= episode.name
+    //   divName.classList.add("divName")
+    //   div.append(divName)
+    //   divSummary= document.createElement("div")
+    //   divSummary.innerHTML = episode.summary
+    //   divSummary.classList.add("divSummary")
+    //   div.append(divSummary)
+    //   divScore = document.createElement("span")
+    //   divScore.innerText = `${episode.rating.average}/10`;
+    //   let a = document.createElement("a")
+    //   a.href=episode.url
+    //   a.classList.add("a")
+    //   divScore.classList.add("divScore")
+    //   div.append(divScore)
+    //   let icon = document.createElement("i")
+    //   icon.classList.add("fas", "fa-play" , "icon");
+    //   a.append(icon)
+    //   div.append(a)
+    //   // console.log(episode)
+    // }
+    let all = allEpisodes;
+    // console.log(all)
+    let allFilter = all.filter(
+      (x) => x.name.includes("Long") || x.summary.includes("Long")
+    );
+    // console.log(allFilter)
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 function creation(allEpisodes) {
   for (episode of allEpisodes) {
@@ -126,12 +168,53 @@ function creation(allEpisodes) {
 }
 // grabber()
 // let select = document.querySelector("select");
-select.addEventListener("click", (e)=>{
+select.addEventListener("click", (e) => {
   console.log(e.target.value);
-  if (e.target.value === "all") {
-    grabber();
+  if (e.target.value === "nothing") {
+    let divs = document.querySelectorAll(".div-card");
+    for (div of divs) {
+      div.remove();
+    }
+  } else if (e.target.value === "all") {
+    grabber(e.target.value);
+    // creation(allEpisodes);
+  } else {
+    let divs = document.querySelectorAll(".div-card");
+    for (div of divs) {
+      div.remove();
+    }
+    grabber(e.target.value);
+    console.log(e.target.value);
   }
-})
+});
+
+let searcher = async (target) => {
+  try {
+    // const res = await axios.get("https://api.tvmaze.com/shows/5/episodes")
+    const res = await axios.get("true.json");
+    // console.log(res.data)
+    allEpisodes = res.data.filter(
+      (x) =>
+        x.name.toLowerCase().includes(target) ||
+        x.name.includes(target) ||
+        x.summary.toLowerCase().includes(target) ||
+        x.summary.includes(target)
+    );
+    console.log(allEpisodes);
+    creation(allEpisodes);
+  } catch (err) {
+    console.log(err);
+  }
+};
+input = document.querySelector("input");
+input.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
+  let divs = document.querySelectorAll(".div-card");
+  for (div of divs) {
+    div.remove();
+  }
+  searcher(e.target.value);
+});
 
 // container.addEventListener("mousemove",(e)=>{
 //     let summary = document.querySelectorAll(".divSummary")
@@ -153,4 +236,3 @@ select.addEventListener("click", (e)=>{
 // number 5 is id of true detective
 
 // for showing seasons we can use https://api.tvmaze.com/shows/5/seasons
-
